257c51def78da0630363a43dddc0c80fa110178f
Minh An Cao <minhan.duc.cao@gmail.com>
2019-11-15 13:53:44 -0800

    Working on live feed component

--- src/components/LiveFeedPage.js
+++ src/components/LiveFeedPage.js
@@ -1,100 +1,43 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import Typography from "@material-ui/core/Typography";
+import React from "react";
+import { Chat, Channel, ChannelHeader, Window } from "stream-chat-react";
+import {
+  MessageList,
+  MessageInput,
+  MessageLivestream
+} from "stream-chat-react";
+import { MessageInputSmall, Thread } from "stream-chat-react";
+import { StreamChat } from "stream-chat";
 
-const useStyles = theme => ({
-  root: {
-    width: "100%",
-    maxWidth: 360,
-    backgroundColor: "blue"
+import "stream-chat-react/dist/css/index.css";
+
+const chatClient = new StreamChat("qk4nn7rpcn75");
+const userToken =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZmFuY3ktbWF0aC03In0.h77C0vYnaWNYVG_1BhYN71UPZxm_O7SGKWU1Pi5ci8Q";
+
+chatClient.setUser(
+  {
+    id: "fancy-math-7",
+    name: "Fancy math",
+    image: "https://getstream.io/random_svg/?id=fancy-math-7&name=Fancy+math"
   },
-  inline: {
-    display: "inline"
-  }
+  userToken
+);
+
+const channel = chatClient.channel("livestream", "spacex", {
+  image: "https://goo.gl/Zefkbx",
+  name: "SpaceX launch discussion"
 });
 
-class LiveFeedPage extends Component {
-  render() {
-    const classes = useStyles();
-
-    return (
-      <div className="App">
-        This is the live feed page.
-        <div>
-          <List className={classes.root}>
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-              </ListItemAvatar>
-              <ListItemText
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      className={classes.inline}
-                      color="textPrimary"
-                    >
-                      Ali Connors
-                    </Typography>
-                    {" — I'll be in your neighborhood doing errands this…"}
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-              </ListItemAvatar>
-              <ListItemText
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      className={classes.inline}
-                      color="textPrimary"
-                    >
-                      to Scott, Alex, Jennifer
-                    </Typography>
-                    {" — Wish I could come, but I'm out of town this…"}
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-              </ListItemAvatar>
-              <ListItemText
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      className={classes.inline}
-                      color="textPrimary"
-                    >
-                      Sandra Adams
-                    </Typography>
-                    {" — Do you have Paris recommendations? Have you ever…"}
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-          </List>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default withStyles(useStyles)(LiveFeedPage);
+const LiveFeedPage = () => (
+  <Chat client={chatClient} theme={"livestream dark"}>
+    <Channel channel={channel} Message={MessageLivestream}>
+      <Window hideOnThread>
+        <ChannelHeader live />
+        <MessageList />
+        <MessageInput Input={MessageInputSmall} focus />
+      </Window>
+      <Thread fullWidth />
+    </Channel>
+  </Chat>
+);
+export default LiveFeedPage;
